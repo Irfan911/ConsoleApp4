e8428d926f318efb8033cca9f0573254f89b3e1d
Irfan911 <73933353+Irfan911@users.noreply.github.com>
2022-04-28 17:31:49 +0530

    first

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp4
             Console.WriteLine("Hello World!");
             Console.Write("hi");
             Console.Write("abcd");  
-            Console.Write("abd");
+           
         }
     }
 }
